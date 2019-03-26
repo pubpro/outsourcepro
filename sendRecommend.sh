90b7c474dbc4b75ce5323f2194c1f19ffed5c1e1
zj519718645 <zj519718645@aliyun.com>
2019-03-26 22:12:20 +0800

    select concpet header

--- sendRecommend.sh
+++ sendRecommend.sh
@@ -16,11 +16,8 @@ mysql -uroot -D tushare -pAbcd1234 -e 'call GetFinalResult(20184);'
 
 echo "send mail ..."
 
-echo "=====================获取新低票===============================">Stock_Recommend.txt
-mysql -uroot -D tushare -pAbcd1234 -e "select t1.code, t1.name from RESULT_RECOMMEND as t1 inner join new_lowprice as t2 on t1.code = t2.code"  >>Stock_Recommend.txt
-
 
 echo "====================短线龙头票=============================">>Stock_Recommend.txt
-mysql -uroot -D tushare -pAbcd1234 -e "select t1.code,t1.name,t1.industry,t1.score from RESULT_RECOMMEND as t1 inner join (select industry,max(score) as score from RESULT_RECOMMEND group by industry) as t2 on t1.industry = t2.industry and t1.score = t2.score" >>Stock_Recommend.txt
+mysql -uroot -D tushare -pAbcd1234 -e "select t1.code,t1.name,t1.industry,t2.c_name,t1.score from RESULT_RECOMMEND as t1 inner join concept_data as t2 on t1.code = t2.code order by t2.c_name, t1.score desc" >>Stock_Recommend.txt
 
 sed 's/\t/,/g' Stock_Recommend.txt | mail -s "Stock Recommend" 519718645@qq.com
